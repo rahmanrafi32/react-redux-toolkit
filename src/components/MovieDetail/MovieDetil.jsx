27dc0975740d91077d313367497a5880bf8395fe
Minhazur Rahman Rafi <rafi.rahman03@yahoo,com>
2021-09-30 00:08:43 +0600

    folder structure done

---
+++ src/components/MovieDetail/MovieDetil.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MovieDetil = () => {
+    return (
+        <div>
+            <h1>Movie details</h1>
+        </div>
+    );
+};
+
+export default MovieDetil;
---
+++ src/components/MovieListnig/MovieListing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MovieListing = () => {
+    return (
+        <div>
+            <h1>Movie List</h1>
+        </div>
+    );
+};
+
+export default MovieListing;
