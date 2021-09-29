bd44b1e80df1ae671034aefd58927901631d9f2d
Minhazur Rahman Rafi <rafi.rahman03@yahoo,com>
2021-09-30 01:02:40 +0600

    sass version problem solved and routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
 
+} from "react-router-dom";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetil";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import './app.scss'
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello Redux</h1>
-        </div>
+        <Router>
+            <Switch>
+                <Route path={"/"} exact component={Home}/>
+                <Route path={"/movie/:imdbID"} component={MovieDetail}/>
+                <Route path={"/*"} component={PageNotFound}/>
+            </Switch>
+        </Router>
     );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import user from '../../images/user-image-icon-17.jpg'
+import './Header.scss'
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className={"header"}>
+            <Link to={"/"}>Movie App</Link>
+            <div className="logo">
+                <div className="user-image">
+                    <img src={user} alt="user"/>
+                </div>
+            </div>
         </div>
     );
 };
