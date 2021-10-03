e455e6bec3d9e47fe6d38c56d7402a71f7fa6fb6
Minhazur Rahman Rafi <rafi.rahman03@yahoo,com>
2021-10-04 00:05:05 +0600

    details page implemented

--- src/components/MovieDetail/MovieDetil.jsx
+++ src/components/MovieDetail/MovieDetil.jsx
@@ -1,19 +1,62 @@
 import React, {useEffect} from "react";
 import {useParams} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
-import {fetchAsyncMoviesorShows, getSelectedMovieOrShow} from "../../features/Movies/MovieSlice";
-
+import {fetchAsyncMoviesorShows, getSelectedMovieOrShow, removeSelectedMovieOrShow} from "../../features/Movies/MovieSlice";
+import './MovieDetail.scss'
 const MovieDetil = () => {
+
     const {imdbID} = useParams();
     const dispatch = useDispatch();
     const data = useSelector(getSelectedMovieOrShow)
-    console.log(data);
+
     useEffect(()=>{
         dispatch(fetchAsyncMoviesorShows(imdbID))
+        return ()=>{
+            dispatch(removeSelectedMovieOrShow())
+        }
     },[dispatch, imdbID])
     return (
-        <div>
-            <h1>Movie details</h1>
+        <div className={"movie-section"}>
+            {Object.keys(data).length === 0 ? <div>...loading</div>:<>
+                <div className="section-left">
+                    <div className="movie-title">
+                        {data.Title}
+                    </div>
+                    <div className="movie-rating">
+                        <span>IMDB Rating:{data.imdbRating} </span>
+                        <span>IMDB Rating:{data.imdbVotes} </span>
+                        <span>IMDB Rating:{data.Runtime} </span>
+                        <span>IMDB Rating:{data.Year} </span>
+                    </div>
+                    <div className="movie-plot">
+                        {data.Plot}
+                    </div>
+                    <div className="movie-info">
+                        <span>Director</span>
+                        <span>{data.Director}</span>
+                    </div>
+                    <div className="movie-info">
+                        <span>Stars</span>
+                        <span>{data.Actor}</span>
+                    </div>
+                    <div className="movie-info">
+                        <span>Genres</span>
+                        <span>{data.Genre}</span>
+                    </div>
+                    <div className="movie-info">
+                        <span>Languages</span>
+                        <span>{data.Language}</span>
+                    </div>
+                    <div className="movie-info">
+                        <span>Awards</span>
+                        <span>{data.Award}</span>
+                    </div>
+                </div>
+                <div className="section-right">
+                    <img src={data.Poster} alt={data.Title}/>
+                </div>
+            </>}
+
         </div>
     );
 };
--- src/features/Movies/MovieSlice.js
+++ src/features/Movies/MovieSlice.js
@@ -35,8 +35,8 @@ const movieSlice = createSlice({
     name: "movies",
     initialState,
     reducers: {
-        addMovies: (state, {payload}) => {
-            state.movies = payload;
+        removeSelectedMovieOrShow:(state)=>{
+            state.selectedMovieOrShow ={}
         }
     },
     extraReducers: {
@@ -62,7 +62,7 @@ const movieSlice = createSlice({
     }
 });
 
-export const {addMovies} = movieSlice.actions;
+export const {removeSelectedMovieOrShow} = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
 export const getAllShows = (state) => state.movies.shows;
 export const getSelectedMovieOrShow = (state) => state.movies.selectedMovieOrShow;
